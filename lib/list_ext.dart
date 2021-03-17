77831df9b83a1ec1627295e0b50c2b17f9c6e7cb
ericmartineau <smartytime@gmail.com>
2021-03-16 23:40:02 -0700

    adding nullable extensions

--- lib/list_ext.dart
+++ lib/list_ext.dart
@@ -1,3 +1,52 @@
+extension NullIterXX<V> on Iterable<V>? {
+  Iterable<R> orMap<R>(R mapper(V)) {
+    if (this == null) return const [];
+    return this!.map(mapper);
+  }
+
+  Iterable<V> orWhere(bool predicate(V v)) {
+    if (this == null) return const [];
+    return this!.where(predicate);
+  }
+
+  bool orContains(V other) {
+    if (this == null) return false;
+    return this!.contains(other);
+  }
+
+  int get orLength {
+    if (this == null) return 0;
+    return this!.length;
+  }
+
+  Iterable<V> orEmptyIter() {
+    if (this == null) return const [];
+    return this!;
+  }
+
+  V? get firstOrNull {
+    return (this == null)
+        ? null
+        : this!.isEmpty
+            ? null
+            : this!.first;
+  }
+}
+
+extension NullListXX<V> on List<V>? {
+  List<V> orEmptyList() {
+    if (this == null) return const [];
+    return this!;
+  }
+}
+
+extension NullSetXX<V> on Set<V>? {
+  Set<V> orEmptySet() {
+    if (this == null) return const {};
+    return this!;
+  }
+}
+
 extension IterXX<V> on Iterable<V?> {
   List<V> notNull() {
     return <V>[
